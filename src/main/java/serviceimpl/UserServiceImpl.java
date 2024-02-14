9a0b04c69d963dba7d64e93715b5b77b92c36f7e
corpseed <132264141+Corpseed2023@users.noreply.github.com>
2024-02-14 11:42:03 +0530

    pom.xml changes

---
+++ src/main/java/serviceimpl/UserServiceImpl.java
@@ -0,0 +1,14 @@
+package serviceimpl;
+
+
+import model.User;
+import org.springframework.stereotype.Service;
+import service.UserService;
+
+@Service
+public class UserServiceImpl implements UserService {
+    @Override
+    public void saveUserData(User user) {
+
+    }
+}
