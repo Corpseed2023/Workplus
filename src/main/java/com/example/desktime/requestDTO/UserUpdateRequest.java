4edfee46f8f06e59336bf2a746eb842bfc94ea40
Corpseed2023 <kaushlendra.pratap@corpseed.com>
2024-04-19 14:20:10 +0530

    Password added and sent on mail

--- src/main/java/com/example/desktime/requestDTO/UserUpdateRequest.java
+++ src/main/java/com/example/desktime/requestDTO/UserUpdateRequest.java
@@ -1,22 +1,21 @@
 package com.example.desktime.requestDTO;
 
-
-
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
 import java.util.Date;
 import java.util.Set;
-
 
 @Getter
 @Setter
 @NoArgsConstructor
-public class UserRequest {
+public class UserUpdateRequest {
 
     private String username;
     private String email;
     private Long createdBy;
+    private String password;
     private Date createdAt;
     private Date updatedAt;
     private boolean isEnable;
