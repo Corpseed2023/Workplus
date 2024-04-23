221e34b9b84ea1323c3f6ddaa4c95dd02d33f5d5
Corpseed2023 <kaushlendra.pratap@corpseed.com>
2024-04-23 14:55:39 +0530

    user proces mail and date added

--- src/main/java/com/example/desktime/controller/UserProcessController.java
+++ src/main/java/com/example/desktime/controller/UserProcessController.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
 import java.time.format.DateTimeParseException;
 import java.util.List;
 
@@ -47,6 +48,10 @@ public class UserProcessController {
         }
     }
 
+    @GetMapping("/getUserDateProcess")
+    public List<UserProcessResponse> getUserDateProcess(@RequestParam String userEmail, @RequestParam LocalDate date) {
+        return userProcessService.getUserProcessWithEmailAndDate(userEmail, date);
+    }
 
 
 }
