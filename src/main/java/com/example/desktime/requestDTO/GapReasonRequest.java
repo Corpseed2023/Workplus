71b7423008b4ebf2ab22b87c06eb747ac2c554ca
Corpseed2023 <kaushlendra.pratap@corpseed.com>
2024-06-25 12:45:56 +0530

    updated reason flag added

---
+++ src/main/java/com/example/desktime/requestDTO/GapReasonRequest.java
@@ -0,0 +1,15 @@
+package com.example.desktime.requestDTO;
+
+
+public class GapReasonRequest {
+
+    private String reason;
+
+    public String getReason() {
+        return reason;
+    }
+
+    public void setReason(String reason) {
+        this.reason = reason;
+    }
+}
