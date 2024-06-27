baf680fd690989f08f119b84082e6a7d1a734ad5
Corpseed2023 <kaushlendra.pratap@corpseed.com>
2024-06-27 17:26:48 +0530

    screenshot time corrected

--- src/main/java/com/example/desktime/serviceimpl/GapTrackServiceImpl.java
+++ src/main/java/com/example/desktime/serviceimpl/GapTrackServiceImpl.java
@@ -8,15 +8,12 @@ import com.example.desktime.repository.UserRepository;
 import com.example.desktime.requestDTO.GapTrackRequest;
 import com.example.desktime.responseDTO.*;
 import com.example.desktime.service.GapTrackService;
-import com.example.desktime.util.CommonUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.example.desktime.ApiResponse.UserNotFoundException;
-
 import java.time.*;
 import java.time.temporal.ChronoUnit;
 import java.util.*;
-import java.util.stream.Collectors;
 
 
 @Service
