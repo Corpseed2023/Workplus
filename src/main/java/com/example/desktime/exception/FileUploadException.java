281a3099cde219b5c0aa476dbb1240b060414556
corpseed <132264141+Corpseed2023@users.noreply.github.com>
2024-07-01 20:37:30 +0530

    excption added for aws

---
+++ src/main/java/com/example/desktime/exception/FileUploadException.java
@@ -0,0 +1,8 @@
+package com.example.desktime.exception;
+
+public class FileUploadException extends SpringBootFileUploadException{
+
+    public FileUploadException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/example/desktime/exception/SpringBootFileUploadException.java
@@ -0,0 +1,7 @@
+package com.example.desktime.exception;
+
+public class SpringBootFileUploadException extends Exception{
+    public SpringBootFileUploadException(String message) {
+        super(message);
+    }
+}
