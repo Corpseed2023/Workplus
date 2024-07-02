d1a9f9dd99879cd4978a73ca2cd6c91c9cb119c5
Corpseed2023 <kaushlendra.pratap@corpseed.com>
2024-07-02 14:46:53 +0530

    AWS Changes Added with all features

---
+++ src/main/java/com/example/desktime/config/AWSS3ClientConfig.java
@@ -0,0 +1,30 @@
+package com.example.desktime.config;
+
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AWSS3ClientConfig {
+
+    @Autowired
+    private AWSCredentialsConfig awsCredentialsConfig;
+
+    @Bean
+    public AmazonS3 initS3Client(){
+        AWSCredentials credentials = new BasicAWSCredentials(
+                awsCredentialsConfig.getAccessKey(),
+                awsCredentialsConfig.getSecretKey()
+        );
+        return AmazonS3ClientBuilder.standard()
+                .withRegion(Regions.AP_SOUTH_1)
+                .withCredentials(new AWSStaticCredentialsProvider(credentials))
+                .build();
+    }
+}
