d2a03f4948c281b18f82818e05ea8f9da7f6762a
Corpseed2023 <kaushlendra.pratap@corpseed.com>
2024-07-05 18:27:50 +0530

    Remove sensitive information from application.properties

--- src/main/java/com/example/desktime/DesktimeApplication.java
+++ src/main/java/com/example/desktime/DesktimeApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DesktimeApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(DesktimeApplication.class, args);
     }
