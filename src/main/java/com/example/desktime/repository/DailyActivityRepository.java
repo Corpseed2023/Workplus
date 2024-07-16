dc1406d7be11b817b4fb036e3497fc4e9e2df04f
Corpseed2023 <kaushlendra.pratap@corpseed.com>
2024-07-16 13:35:38 +0530

    Gap status will update logout time

--- src/main/java/com/example/desktime/repository/DailyActivityRepository.java
+++ src/main/java/com/example/desktime/repository/DailyActivityRepository.java
@@ -2,31 +2,28 @@ package com.example.desktime.repository;
 
 import com.example.desktime.model.DailyActivity;
 import com.example.desktime.model.User;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface DailyActivityRepository  extends JpaRepository<DailyActivity,Long> {
 
-
-    Optional<DailyActivity> findByUser(User user);
-
-    DailyActivity findByUserEmail(String email);
-
-    //complete the method here and also put limit so it can fetch single data for this mail id and current date
-
-//    @Query(value = "Select * from daily_activity da where da.user = : (select id from user where email =: email) and da.date= : date limit 1" ,nativeQuery = true)
-//    DailyActivity findByUserEmailAndDate(@Param("email") String email, @Param("date") LocalDate date);
+    @Modifying
+    @Transactional
+    @Query(value = "update daily_activity da JOIN user u On da.user_id = u.id set da.logout_time= :logoutTime where u.email = :email  and da.date = :date",nativeQuery = true)
+    void updateLogoutTime(@Param("email") String email, @Param("date") LocalDate date, @Param("logoutTime") LocalDateTime logoutTime);
 
     @Query(value = "SELECT * FROM daily_activity da WHERE da.user_id = (SELECT id FROM user WHERE email = :email) AND da.date = :date LIMIT 1", nativeQuery = true)
     DailyActivity findByUserEmailAndDate(@Param("email") String email, @Param("date") LocalDate date);
-
 
     @Query(value = "SELECT da FROM DailyActivity da WHERE da.user = :user AND da.date = :date")
     Optional<DailyActivity> findByUserAndDate(@Param("user") User user, @Param("date") LocalDate date);
